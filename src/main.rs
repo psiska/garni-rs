e25e62942dc3f98b6d50af99101b23fbeb8e842e
Peter Siska <siska.pe@gmail.com>
2026-07-06 18:58:33 +0200

    feat: Use tracing for logging

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use axum::http::{
 use route::create_router;
 use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{EnvFilter};
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
@@ -20,13 +21,13 @@ async fn main() {
         // when running the app to set log levels.
         .with_env_filter(
             EnvFilter::try_from_default_env()
-                .or_else(|_| EnvFilter::try_new("axum_tracing_example=info,tower_http=info"))
+                .or_else(|_| EnvFilter::try_new("garni_rs=info,axum_tracing_example=info,tower_http=info"))
                 .unwrap(),
         )
         .init();
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin("http://localhost:8305".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
@@ -35,7 +36,8 @@ async fn main() {
         .layer(cors)
         .layer(TraceLayer::new_for_http());
 
-    println!("🚀 Server started successfully");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    info!("garni-rs:server: started");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8305").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
